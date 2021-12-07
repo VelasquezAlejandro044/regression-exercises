10265c985a5cc6d2aef44b79fe54f7df374a8bcc
Alejandro Velasquez <VelasquezAlejandro044@gmail.com>
2021-12-07 04:06:46 -0500

    up

--- wrangle.py
+++ wrangle.py
@@ -12,6 +12,17 @@ from sklearn.impute import SimpleImputer
 
 from env import user, password, host
 
+
+#******************************Get Connection*******************
+
+def get_connection(db):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    It takes in a string name of a database as an argument.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
 #**************************************************Acquire*******************************************************
 
 def acquire_zillow():
@@ -197,3 +208,41 @@ def Min_Max_Scaler(X_train, X_validate, X_test):
     X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
     
     return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+
+#************************* Telco Data *******************************
+
+def new_telco_data():
+    '''
+    This function reads the iris data from the Codeup db into a df.
+    '''
+    sql_query = """
+                select * from customers
+                join contract_types using (contract_type_id)
+                join internet_service_types using (internet_service_type_id)
+                join payment_types using (payment_type_id)
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+def get_telco_data():
+    '''
+    This function reads in iris data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('telco.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('telco.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_telco_data()
+        
+        # Cache data
+        df.to_csv('telco.csv')
+        
+    return df
